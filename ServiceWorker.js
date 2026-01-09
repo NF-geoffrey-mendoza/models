7fa16ee2e4734f81beea01bab2eaeb35d5de045c
geoffrey mendoza <geoffrey_mendoza@neun.ph>
2026-01-09 13:21:03 +0800

    Initial Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Neun Farben Corporation-models-kyuu-iroha-project-0.1.0";
+const contentToCache = [
+    "Build/Test_Beach6.loader.js",
+    "Build/Test_Beach6.framework.js.gz",
+    "Build/Test_Beach6.data.gz",
+    "Build/Test_Beach6.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
